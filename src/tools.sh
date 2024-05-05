0471592a21cc514adc5b001b46225495c88fa6fd
Leon Blakey <code@leonblakey.com>
2024-05-05 12:26:19 -0700

    new function zfstooglero

--- src/tools.sh
+++ src/tools.sh
@@ -21,3 +21,27 @@ linklevels() {
         echo "new $file"
     done
 }
+
+zfstogglero() {
+  volume="$1"
+  if [ -z "$volume" ]; then
+    echo "need volume"
+    return 1
+  fi
+
+  if [ "$(zfs get readonly -Ho value $volume)" = "on" ]; then
+    set -x
+    zfs set readonly=off $volume
+    set +x
+  else
+    set -x
+    zfs inherit readonly $volume
+    set +x
+
+    if [ "$(zfs get readonly -Ho value $volume)" != "on" ]; then
+      echo "readonly still off from parent!"
+      return 5
+    fi
+
+  fi
+}
