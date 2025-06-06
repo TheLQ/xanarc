fbf0038ef7bff15c423096273577dcc5e1423431
Leon Blakey <code@leonblakey.com>
2025-06-05 19:14:28 -0700

    ps1: title attempt cleanup

--- src/ps1.sh
+++ src/ps1.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-# # set variable identifying the chroot you work in (used in the prompt below)
-# if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-#     debian_chroot=$(cat /etc/debian_chroot)
-# fi
-
-# # set a fancy prompt (non-color, unless we know we "want" color)
-# case "$TERM" in
-#     xterm-color|*-256color) color_prompt=yes;;
-# esac
-
-# if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-# 	# We have color support; assume it's compliant with Ecma-48
-# 	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-# 	# a case would tend to support setf rather than setaf.)
-# 	color_prompt=yes
-# else
-# 	color_prompt=
-# fi
-
-function __prompt_title() {
-    # Set terminal title
-    echo -en "\e]0;$USER@$HOSTNAME"
-}
+# The dark arts of terminal escape sequences
+#
+# Simple: https://ghostty.org/docs/vt/concepts/sequences
+# Reference: https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+# \e[ - ANSI Escape Sequences
+# \e] - OSC Escape Sequences
 
 PROMPT_COMMAND=__prompt_command
 __prompt_command() {
@@ -65,24 +48,24 @@ __prompt_command() {
     local __prompt_host="${start}${__prompt_host_color}${end}${__prompt_host_text}${start}${clear}${end}"
 
     local __prompt_dir="${start}${blue}${end}\w${start}${clear}${end}"
-    
 
-    # ${debian_chroot:+($debian_chroot)}
-#   if [ "$color_prompt" = yes ]; then
-       PS1="${__prompt_user}@${__prompt_host}:${__prompt_dir}\$ "
-#   else
-#       PS1='\u@\h:\w\$ '
-#   fi
+    # https://tldp.org/HOWTO/Bash-Prompt-HOWTO/xterm-title-bar-manipulations.html
+    # http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#toc4
+    # https://chromium.googlesource.com/apps/libapps/+/a5fb83c190aa9d74f4a9bca233dac6be2664e9e9/hterm/doc/ControlSequences.md#OSC
+    # local __title_bar="${start}\e]2;X\u@\h\007${end}"
 
-   if [ "$curr_exit" != 0 ]; then
-       PS1="${start}${red}${end}$curr_exit${start}${clear}${end} $PS1"
-   fi
+    # Set tmux-specific title
+    # https://manpages.debian.org/bookworm/tmux/tmux.1.en.html#NAMES_AND_TITLES
+    # local __title_bar="\ek$USER@$HOSTNAME\e\\"
+
+    # final config
+    PS1="${__title_bar}${__prompt_user}@${__prompt_host}:${__prompt_dir}\$ "
+    if [ "$curr_exit" != 0 ]; then
+        PS1="${start}${red}${end}$curr_exit${start}${clear}${end} $PS1"
+    fi
 
     # append missing current session history into .bash_history
     history -a
     # append missing .bash_history entries into current session
     history -n
-
-    # if a child chroot/lxc exits, reset the terminal title
-   __prompt_title
 }
